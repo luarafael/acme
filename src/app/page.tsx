e6f3f73e95d6bbe4002a94cf65614b9e1a070ded
luarafael <luarvb12@gmail.com>
2026-03-10 13:14:20 +0000

    style: adicionar estilizacao temporaria

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
 export default function Page() {
-    return 'Sistema em construção.'
+    return (
+        <main className="bg-black h-screen flex 
+        justify-center items-center">
+            <h1 className="text-blue-800 font-semibold
+            text-4x1">Sistema em construção.</h1>
+        </main>
+    )
 }
