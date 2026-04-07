90bd78693c91bc6a7515adf1372d6e5400497c98
luarafael <luarvb12@gmail.com>
2026-04-07 11:56:35 +0000

    chore: instalar dependencia de validador de dados

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,27 +1,27 @@
- export interface User {
+export interface User {
   id: string;
   name: string;
   email: string;
   password: string;
 };
 
- export type CreateUserData = Omit<User, 'id'>;
- export type UpdateUserData = Partial<CreateUserData>;
+export type CreateUserData = Omit<User, 'id'>;
+export type UpdateUserData = Partial<CreateUserData>;
 
 
- export interface Customer {
+export interface Customer {
   id: string;
   name: string;
   email: string;
   imageUrl: string;
 };
 
- export type CreateCustomerData = Omit<Customer, 'id'>;
- export type UpdateCustomerData = Partial<CreateCustomerData>;
+export type CreateCustomerData = Omit<Customer, 'id'>;
+export type UpdateCustomerData = Partial<CreateCustomerData>;
 
- export type InvoiceStatus = 'PENDENTE' | 'PAGO'
+export type InvoiceStatus = 'PENDENTE' | 'PAGO'
 
- export interface Invoice {
+export interface Invoice {
   id: string;
   customer_id: string;
   amount: number;
@@ -29,22 +29,22 @@
   status: InvoiceStatus;
 };
 
- export type CreateInvoiceData = Omit<Invoice, 'id'>;
- export type UpdateInvoiceData = Partial<CreateInvoiceData>;
+export type CreateInvoiceData = Omit<Invoice, 'id'>;
+export type UpdateInvoiceData = Partial<CreateInvoiceData>;
 
- export interface Revenue {
+export interface Revenue {
   month: string;
   revenue: number;
- };
- 
- export interface ApiResponse<T>{
+};
+
+export interface ApiResponse<T> {
   data: T;
   message?: string;
- };
+};
 
- export interface ApiError {
- error: string;
- details?: unknown;
- };
+export interface ApiError {
+  error: string;
+  details?: unknown;
+};
 
- 
+
