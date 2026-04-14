255a4bc0514750a3c33b583364a93cfd94dd9182
luarafael <luarvb12@gmail.com>
2026-04-14 12:03:19 +0000

    chore: criando arquivo route.ts

---
+++ src/app/api/customers/routes.ts
@@ -0,0 +1,33 @@
+import {
+  NextRequest,
+  NextResponse
+} from 'next/server';
+
+import { CustomerController } from '@/controllers/CustomerController';
+
+const {
+  getALL,
+  create
+} = CustomerController
+
+export async function GET(request: NextRequest) {
+  const { searchParams } = request.nextUrl;
+
+  const result = await getALL(searchParams)
+
+  return NextResponse.json(
+    result.body,
+    { status: result.status }
+  );
+};
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+
+  const result = await create(body);
+
+  return NextResponse.json(
+    result.body,
+    { status: result.status }
+  );
+};
